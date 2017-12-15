d15be5c5d8c894de8b04a5ec1a16e8483f2c0e27
Larry-Hignight <larry.hignight@gmail.com>
2017-12-15 08:43:32 +0000

    Changed the comments to echo statements in the github-setup.sh file.

--- github-setup.sh
+++ github-setup.sh
@@ -1,9 +1,9 @@
-# Set the globel user.name and user.email
+echo "==== Setting the globel user.name and user.email ===="
 git config --global user.name "Larry-Hignight"
 git config --global user.email "larry.hignight@gmail.com"
 
-# Set git to use the credential memory cache
+echo "==== Setting git to use the credential memory cache ===="
 git config --global credential.helper cache
 
-# Set the cache to timeout after a very long time
+echo "==== Setting the cache to timeout after a very long time ===="
 git config --global credential.helper 'cache --timeout=9999999999999'
