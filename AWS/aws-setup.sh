dfa422dfb82249a207026cdf048fa45917de9db5
Larry-Hignight <larry.hignight@gmail.com>
2017-12-18 02:00:56 +0000

    Uncommented some lines in the aws-setup.sh file that shouldn't have been commented out.

--- AWS/aws-setup.sh
+++ AWS/aws-setup.sh
@@ -1,11 +1,11 @@
-#echo "==== Upgrading the system ===="
-#sudo apt-get update && sudo apt-get -y upgrade
-#
-#echo "==== Installing software for AWS ===="
-#aws-install.sh
+echo "==== Upgrading the system ===="
+sudo apt-get update && sudo apt-get -y upgrade
 
-#echo "==== Running Github Setup Script ===="
-#../github-setup.sh
+echo "==== Installing software for AWS ===="
+aws-install.sh
+
+echo "==== Running Github Setup Script ===="
+../github-setup.sh
 
 echo "==== Installing Docker and Docker Compose ===="
 ../Docker/docker-ce-install-ubuntu.sh
