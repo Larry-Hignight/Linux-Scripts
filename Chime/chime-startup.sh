d2d49904ed4572ab81eaced49bc61dd9375ff16b
Larry-Hignight <larry.hignight@gmail.com>
2018-01-16 15:08:46 -0800

    Chime: Initial commit of a startup script for Chime to run on chime.

---
+++ Chime/chime-startup.sh
@@ -0,0 +1,9 @@
+echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
+
+echo "setting the font" && setfont /usr/share/consolefonts/Lat15-VGA8.psf.gz && echo "successfull"
+
+echo "unsetting auto logout" && unset autologout && echo "successfull"
+
+echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/scripts/tty-script.sh && echo "successfull"
+
+echo "mounting bell" && sh /home/chime/scripts/mount-bell.sh && echo "successfull"
