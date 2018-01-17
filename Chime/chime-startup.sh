08bd2bc32a3e83322ff09e8eaa23e4f078df095a
Larry-Hignight <larry.hignight@gmail.com>
2018-01-17 13:39:00 -0800

    Chime:  Added dot to the path statement;  Mounts the directories from bell before opening vts on chime.

--- Chime/chime-startup.sh
+++ Chime/chime-startup.sh
@@ -4,8 +4,11 @@ echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
 
 echo "setting the font" && setfont /usr/share/consolefonts/Lat15-VGA8.psf.gz && echo "successfull"
 
-echo "unsetting auto logout" && unset autologout && echo "successfull"
+echo "mounting bell" && sh /home/chime/scripts/mount-bell.sh && echo "successfull"
 
 echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/scripts/tty-script.sh && echo "successfull"
 
-echo "mounting bell" && sh /home/chime/scripts/mount-bell.sh && echo "successfull"
+echo "adding dot to the path - a terrible idea"
+    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:."
+    && echo "successfull"
+    
