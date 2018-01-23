27bfe7d038f0fa705b6fe9f4966afafece87921b
Ubuntu <ubuntu@ip-172-31-12-138.us-west-1.compute.internal>
2018-01-23 02:39:28 +0000

    AWS: Changed the aws-install.sh file to use apt instead of apt-get for installing packages.

--- AWS/aws-install.sh
+++ AWS/aws-install.sh
@@ -1,54 +1,54 @@
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt update && sudo apt -y upgrade
 
 echo "################################"
 echo "## Setup a working Python Env ##"
 echo "################################"
 
-sudo apt-get -y install python3-pip
-sudo apt-get -y install python3-venv
+sudo apt -y install python3-pip
+sudo apt -y install python3-venv
 pip3 install --upgrade pip
 
 
 echo "#######################"
 echo "## Development Tools ##"
 echo "#######################"
-sudo apt-get -y install emacs24-nox
+sudo apt -y install emacs24-nox
 
 
 echo "##############################"
 echo "## R & RStudio Installation ##"
 echo "##############################"
-sudo apt-get -y install r-base                # Note - I changed this from r-base-core; TODO: Review these packages
-sudo apt-get -y install libjpeg62             # Required for RStudio
-sudo apt-get -y install libssl-dev            # Required for the httr and openssl packages
-sudo apt-get -y install libcurl4-openssl-dev  # Required for the RCurl package
-sudo apt-get -y install libxml2-dev           # Required for the XML package
+sudo apt -y install r-base                # Note - I changed this from r-base-core; TODO: Review these packages
+sudo apt -y install libjpeg62             # Required for RStudio
+sudo apt -y install libssl-dev            # Required for the httr and openssl packages
+sudo apt -y install libcurl4-openssl-dev  # Required for the RCurl package
+sudo apt -y install libxml2-dev           # Required for the XML package
 
 
 echo "#######################################################"
 echo "## Hardware Info / OS Info                           ##"
 echo "## http://www.binarytides.com/linux-cpu-information  ##"
 echo "#######################################################"
-sudo apt-get -y install conky
-sudo apt-get -y install hardinfo
-sudo apt-get -y install cpuid
-sudo apt-get -y install inxi
-sudo apt-get -y install htop
-sudo apt-get -y install pmount  # A handy tool for mounting hardware (eg thumb drives)
-sudo apt-get -y install wavemon # A cool ncurses based wifi monitor
-#sudo apt-get -y install iotop  # Requires a sudoer to run it with root level access
+sudo apt -y install conky
+sudo apt -y install hardinfo
+sudo apt -y install cpuid
+sudo apt -y install inxi
+sudo apt -y install htop
+sudo apt -y install pmount  # A handy tool for mounting hardware (eg thumb drives)
+sudo apt -y install wavemon # A cool ncurses based wifi monitor
+#sudo apt -y install iotop  # Requires a sudoer to run it with root level access
 
 
 echo "##################"
 echo "## Web Browsers ##"
 echo "##################"
-sudo apt-get -y install lynx
+sudo apt -y install lynx
 
 
 # echo "######################"
 # echo "## Java programming ##"
 # echo "######################"
-# sudo apt-get -y install openjdk-8-jdk
-# sudo apt-get -y install ant
-# sudo apt-get -y install maven2
+# sudo apt -y install openjdk-8-jdk
+# sudo apt -y install ant
+# sudo apt -y install maven2
 
