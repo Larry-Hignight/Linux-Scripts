fb0e768ccd2b4d880aca2ad31d5d782728f78196
Larry-Hignight <larry.hignight@gmail.com>
2018-03-29 20:22:53 -0700

    Chime: Updated the chime-startup.sh script to use the current directory structure.

--- Chime/chime-startup.sh
+++ Chime/chime-startup.sh
@@ -1,13 +1,13 @@
-echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/scripts/inflection.sh && echo "successfull"
+echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
 
 echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
 
 echo "setting the font" && setfont /usr/share/consolefonts/Lat15-VGA8.psf.gz && echo "successfull"
 
-echo "mounting bell" && sh /home/chime/scripts/mount-bell.sh && echo "successfull"
+echo "mounting bell" && sh /home/chime/Linux-Scripts/Chime/mount-bell.sh && echo "successfull"
 
-echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/scripts/tty-script.sh && echo "successfull"
+echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/tty-script.sh && echo "successfull"
 
 echo "adding home chime scripts to the path"
-    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/chime/scripts"
+    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/chime/Linux-Scripts/Chime"
     && echo "successfull"
