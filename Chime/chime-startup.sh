02c9948f5e0fc3617158547951e9937388b88cf7
Larry-Hignight <larry.hignight@gmail.com>
2018-03-29 20:39:23 -0700

    Chime: Work around because the inflection script call isn't working in the startup script.

--- Chime/chime-startup.sh
+++ Chime/chime-startup.sh
@@ -1,4 +1,4 @@
-echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
+#echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
 
 echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
 
@@ -8,6 +8,9 @@ echo "mounting bell" && sh /home/chime/Linux-Scripts/Chime/mount-bell.sh && echo
 
 echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/tty-script.sh && echo "successfull"
 
-echo "adding home chime scripts to the path"
-    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/chime/Linux-Scripts/Chime"
+echo "adding home chime scripts to the path" \
+    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/chime/Linux-Scripts/Chime" \
     && echo "successfull"
+
+echo "Cannot run the infliction script -- Must be run as root user"
+cd /home/chime/Linux-Scripts/Chime
