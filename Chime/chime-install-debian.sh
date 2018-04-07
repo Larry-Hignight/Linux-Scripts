496d6fa591c7cff503aae27476e50869b2ffc832
Larry-Hignight <larry.hignight@gmail.com>
2018-04-06 21:19:38 -0700

    Chime:  Improving the output from the install scripts.

--- Chime/chime-install-debian.sh
+++ Chime/chime-install-debian.sh
@@ -1,36 +1,36 @@
-echo "Add Chime to the sudoers file"
-echo ""
+echo "Add Chime to the sudoers file" && echo ""
 su -m
 usermod -aG sudo username chime
 apt-get install sudo
 exit  # Exit from su
+echo "" && echo "Finished adding Chime to the sudoers file" && sleep 3
 
-echo "Setting the font to a higher resolution font (ie more lines per screen)"
-echo ""
+echo "Setting the font to a higher resolution font (ie more lines per screen)" && echo ""
 sh chime-install-setfont.sh
+echo "" && echo "Finished setting the font to a higher resolution font (ie more lines per screen)" && sleep 3
 
-echo "Installing the speakup software for the DEC Talk Express"
-echo ""
+echo "Installing the speakup software for the DEC Talk Express" && echo ""
 sh chime-install-dectalk.sh
+echo "" && echo "Finished installing the speakup software for the DEC Talk Express" && sleep 3
 
-echo "Installing the application software"
-echo ""
+echo "Installing the application software" && echo ""
 sh chime-install-applications.sh
+echo "" && echo "Finished installing the application software" && sleep 3
 
-echo "Installing Docker"
-echo ""
+echo "Installing Docker" && echo ""
 sh ../Docker/docker-ce-debian-install.sh
+echo "" && echo "Finished installing Docker" && sleep 3
 
-echo "Installing tcsh as the default shell"
-echo ""
+echo "Installing tcsh as the default shell" && echo ""
 sh chime-install-tcsh.sh
+echo "" && echo "Finished installing tcsh as the default shell" && sleep 3
 
-echo "Mounting file server using sshfs"
-echo ""
+echo "Mounting file server using sshfs" && echo ""
 sh mount-bell.sh
+echo "" && echo "Finished mounting file server using sshfs" && sleep 3
 
-echo "Removing old packages"
-echo ""
+echo "Removing old packages" && echo ""
 sudo apt autoremove
+echo "" && echo "Finished removing old packages" && sleep 3
 
 echo "Finished"
--- Chime/chime-install-ubuntu.sh
+++ Chime/chime-install-ubuntu.sh
@@ -1,30 +1,36 @@
 echo "Setting the font to a higher resolution font (ie more lines per screen)"
-echo ""
 sh chime-install-setfont.sh
+echo "" && echo "Finished setting the font to a higher resolution font (ie more lines per screen)" && sleep 3
 
 echo "Installing the speakup software for the DEC Talk Express"
 echo ""
 sh chime-install-dectalk.sh
+echo "" && echo "Finished installing the speakup software for the DEC Talk Express" && sleep 3
 
 echo "Installing the application software"
 echo ""
 sh chime-install-applications.sh
+echo "" && echo "Finished installing the application software" && sleep 3
 
 echo "Installing Docker"
 echo ""
 sh ../Docker/docker-ce-install-ubuntu.sh
 sh ../Docker/docker-compose-install-ubuntu.sh
+echo "" && echo "Finished installing Docker" && sleep 3
 
 echo "Installing tcsh as the default shell"
 echo ""
 sh chime-install-tcsh.sh
+echo "" && echo "Finished installing tcsh as the default shell" && sleep 3
 
 echo "Mounting file server using sshfs"
 echo ""
 sh mount-bell.sh
+echo "" && echo "Finished mounting file server using sshfs" && sleep 3
 
 echo "Removing old packages"
 echo ""
 sudo apt autoremove
+echo "" && echo "Finished removing old packages" && sleep 3
 
-echo "Finished"
+echo "" && echo "Finished Installation"
