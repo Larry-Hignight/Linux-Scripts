3a500a9fd3c3c3218490c915e787365ab510fc1e
Larry-Hignight <larry.hignight@gmail.com>
2018-04-07 00:22:07 -0700

    Chime: Install the firefox-esr package on debian.

--- Chime/chime-install-debian.sh
+++ Chime/chime-install-debian.sh
@@ -5,4 +5,8 @@ apt-get install sudo
 exit  # Exit from su
 echo "" && echo "Finished adding Chime to the sudoers file" && sleep 3
 
+## The Ubuntu and Debian installation scripts are nearly the same from this point forward
 sh chime-install-ubuntu.sh
+
+## Install the 'esr' version of Firefox
+sudo apt-get install -y firefox-esr
