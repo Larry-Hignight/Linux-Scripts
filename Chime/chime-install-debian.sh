fdba451c70bced2b3f026098970391f3ea8ce681
Larry-Hignight <larry.hignight@gmail.com>
2018-04-06 21:03:42 -0700

    Chime:  Fixed a bug in the install scripts.

--- Chime/chime-install-debian.sh
+++ Chime/chime-install-debian.sh
@@ -7,23 +7,23 @@ exit
 
 echo "Setting the font to a higher resolution font (ie more lines per screen)"
 echo ""
-source chime-install-setfont.sh
+sh chime-install-setfont.sh
 
 echo "Installing the speakup software for the DEC Talk Express"
 echo ""
-source chime-install-dectalk.sh
+sh chime-install-dectalk.sh
 
 echo "Installing the application software"
 echo ""
-source chime-install-applications.sh
+sh chime-install-applications.sh
 
 echo "Installing tcsh as the default shell"
 echo ""
-source chime-install-tcsh.sh
+sh chime-install-tcsh.sh
 
 echo "Mounting file server using sshfs"
 echo ""
-source mount-bell.sh
+sh mount-bell.sh
 
 echo "Removing old packages"
 echo ""
