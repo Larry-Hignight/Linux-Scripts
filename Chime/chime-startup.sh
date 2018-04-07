3f59876be92da6d9199dd0a6af2dd99d4471f346
Larry-Hignight <larry.hignight@gmail.com>
2018-04-06 23:35:28 -0700

    Chime:  Removed the path statements from the chime-startup script.

--- Chime/chime-startup.sh
+++ Chime/chime-startup.sh
@@ -1,4 +1,4 @@
-#echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
+echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
 
 echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
 
@@ -7,10 +7,3 @@ echo "setting the font" && setfont /usr/share/consolefonts/Lat15-VGA8.psf.gz && 
 echo "mounting bell" && sh /home/chime/Linux-Scripts/Chime/mount-bell.sh && echo "successfull"
 
 echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/tty-script.sh && echo "successfull"
-
-echo "adding home chime scripts to the path" \
-    && set PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/chime/Linux-Scripts/Chime" \
-    && echo "successfull"
-
-echo "Cannot run the infliction script -- Must be run as root user"
-cd /home/chime/Linux-Scripts/Chime
