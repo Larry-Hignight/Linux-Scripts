ab050747442415024fef433751fe279a505a6fec
Larry-Hignight <larry.hignight@gmail.com>
2018-08-30 20:21:47 -0700

    Added a find like tool called fd;  Added the todo section at the top.

--- new-cli-tools.sh
+++ new-cli-tools.sh
@@ -5,6 +5,9 @@
 ## https://remysharp.com/2018/08/23/cli-improved
 ## https://hackaday.com/2018/08/29/linux-fu-modernize-your-command-line/
 ##
+## TODO
+## Oh-My-Zsh (https://medium.com/wearetheledger/oh-my-zsh-made-for-cli-lovers-installation-guide-3131ca5491fb)
+## Highlight (http://www.andre-simon.de/doku/highlight/en/highlight.php)
 
 
 ##################################
@@ -41,3 +44,17 @@ sudo apt-get install oping
 # prettyping is a TUI for ping
 # Like oping/noping, the output is pretty cool, but the installation and setup just aren't worth the effort outside of a demo
 # I don't see much value in using either prettyping or oping.  While noping is pretty cool, if it doesn't hose your terminal.
+
+
+############################
+## FIND like programs: fd ##
+############################
+
+# fd is an "opinionated" alternative to the find command
+# The author states that it is intended to cover the most common 80% of find use-cases
+# Github: https://github.com/sharkdp/fd/
+
+# This is a pretty cool program that I want to continue using in a container before installing on my host system
+# The --exec and multiple threads are very nice features
+wget https://github.com/sharkdp/fd/releases/download/v7.1.0/fd_7.1.0_amd64.deb
+dpkg -i fd_7.1.0_amd64.deb 
