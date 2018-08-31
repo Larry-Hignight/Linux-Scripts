c04271329f2979d287f1dd736a5e6eb029b240a1
Larry-Hignight <larry.hignight@gmail.com>
2018-08-30 20:36:53 -0700

    Alphabetized by the tools that the new tools are most similar too.

--- new-cli-tools.sh
+++ new-cli-tools.sh
@@ -26,6 +26,20 @@ sudo dpkg -i bat_0.6.0_amd64.deb
 # I haven't had an opportunity to install this app
 
 
+############################
+## FIND like programs: fd ##
+############################
+
+# fd is an "opinionated" alternative to the find command
+# The author states that it is intended to cover the most common 80% of find use-cases
+# Github: https://github.com/sharkdp/fd/
+
+# This is a pretty cool program that I want to continue using in a container before installing on my host system
+# The --exec and multiple threads are very nice features
+wget https://github.com/sharkdp/fd/releases/download/v7.1.0/fd_7.1.0_amd64.deb
+dpkg -i fd_7.1.0_amd64.deb 
+
+
 #######################################################
 ## PING like programs: mtr, oping/noping, prettyping ##
 #######################################################
@@ -44,17 +58,3 @@ sudo apt-get install oping
 # prettyping is a TUI for ping
 # Like oping/noping, the output is pretty cool, but the installation and setup just aren't worth the effort outside of a demo
 # I don't see much value in using either prettyping or oping.  While noping is pretty cool, if it doesn't hose your terminal.
-
-
-############################
-## FIND like programs: fd ##
-############################
-
-# fd is an "opinionated" alternative to the find command
-# The author states that it is intended to cover the most common 80% of find use-cases
-# Github: https://github.com/sharkdp/fd/
-
-# This is a pretty cool program that I want to continue using in a container before installing on my host system
-# The --exec and multiple threads are very nice features
-wget https://github.com/sharkdp/fd/releases/download/v7.1.0/fd_7.1.0_amd64.deb
-dpkg -i fd_7.1.0_amd64.deb 
