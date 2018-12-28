6213b56c2226bc9f59f6463422352a1db567990c
Larry-Hignight <larry.hignight@gmail.com>
2018-12-28 00:02:50 -0700

    Changes that Marc made to the startup scripts in early 2018.

--- Chime/Startup-Scripts/chime-startup.sh
+++ Chime/Startup-Scripts/chime-startup.sh
@@ -1,9 +1,7 @@
-echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successfull"
-
-echo "loading pcspkr module" && sudo modprobe pcspkr && echo "successfull"
+echo "running the inflection script" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/inflection.sh && echo "successful"
 
 echo "setting the font" && setfont /usr/share/consolefonts/Lat15-VGA8.psf.gz && echo "successfull"
 
-echo "mounting bell" && sh /home/chime/Linux-Scripts/Chime/mount-bell.sh && echo "successfull"
+echo "mounting bell" && sudo mount -a && echo "mount successful"
 
-echo "opening terminals 2 thru 24" && echo "root-beer" | sudo -S sh /home/chime/Linux-Scripts/Chime/tty-script.sh && echo "successfull"
+sudo cp /home/chime/linux-scripts/characters /sys/accessibility/speakup/i18n/
--- Chime/Startup-Scripts/tty-script.sh
+++ Chime/Startup-Scripts/tty-script.sh
@@ -1,51 +1,7 @@
 #!/bin/sh
 
-# In order for this script to work, set NAutoVTs=1 in the /etc/systemd/logind.conf file
-#Consolse and Shellworld on TTY1
-openvt -u -l -f -c 2   #check current time
-openvt -u -l -f -c 3   #sometimes ncftp
-openvt -u -l -f -c 4   #playing mp3s
-openvt -u -l -f -c 5   #w or who
-openvt -u -l -f -c 6   #lynx www.dxworld.com/tvfmlog.html
-openvt -u -l -f -c 13  #sitting in videos/tv-news
-openvt -u -l -f -c 18  #playing videos/music
-openvt -u -l -f -c 23  #playing video streams with xvfb-run
-
-# These TTYs don't have a predefined role
-openvt -u -l -f -c 7
-openvt -u -l -f -c 8
-openvt -u -l -f -c 9
-openvt -u -l -f -c 10
-openvt -u -l -f -c 11
-openvt -u -l -f -c 12
-openvt -u -l -f -c 14
-openvt -u -l -f -c 15
-openvt -u -l -f -c 16
-openvt -u -l -f -c 17
-openvt -u -l -f -c 19
-openvt -u -l -f -c 20
-openvt -u -l -f -c 21
-openvt -u -l -f -c 22
-openvt -u -l -f -c 24
-
-
-
-
-#chvt 4 && cd mp3
-#chvt 13 && cd videos/tv-news
-#chvt 18 && cd videos/music
-#chvt 1
-
-
-
-#vt-log - console error log
-#vt-sw - Shellworld
-#vt-time - check current time
-#vt-ftp - sometimes ncftp
-#vt-mp3 - playing mp3s
-#vt-who - w or who
-#vt-tv - lynx www.dxworld.com/tvfmlog.html
-#vt-news - sitting in videos/tv-news
-#vt-music - playing videos/music
-#vt-bin - sitting in /home/chime/bin
-#vt-streams - playing video streams with xvfb-run
+for i in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+do
+#  echo "opening terminal $i"
+#  openvt -u
+done
