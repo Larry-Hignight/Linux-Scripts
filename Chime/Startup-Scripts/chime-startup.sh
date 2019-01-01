3f39786c6e015ec636e502d70695d763f2d3f335
Larry-Hignight <larry.hignight@gmail.com>
2019-01-01 14:11:51 -0700

    The inflection alias doesn't work inside of a shell script;  Added the missing /bin to the Speakup customization script.

--- Chime/Startup-Scripts/chime-startup.sh
+++ Chime/Startup-Scripts/chime-startup.sh
@@ -1,7 +1,7 @@
-echo "running the inflection script" && inflection && echo "successful"
+echo "running the inflection script" && sudo sh /usr/local/bin/inflection.sh && echo "successful"
 
 echo "setting tty1 to 135 lines" && stty rows 135 && echo "successful"
 
 echo "mounting bell" && sudo mount -a && echo "successful"
 
-echo "customizing typing speech in speakup" && sudo cp /usr/local/characters /sys/accessibility/speakup/i18n/ && echo "successful"
+echo "customizing typing speech in speakup" && sudo cp /usr/local/bin/characters /sys/accessibility/speakup/i18n/ && echo "successful"
