3381c1b88fe0c7353c11f935fb314cb8cc612311
Larry-Hignight <larry.hignight@gmail.com>
2019-02-03 23:45:42 -0800

    Created a Snap installation script.

---
+++ snap-install.sh
@@ -0,0 +1,27 @@
+# Basic info on using Snaps - https://itsfoss.com/use-snap-packages-ubuntu-16-04/
+
+# Check back to see if Snaps for the following:  Google Chrome, Google Earth, RStudio
+# At some point, I'd like to try the DataGrip IDE by JetBrains - https://snapcraft.io/datagrip
+
+## Communication Tools ##
+
+sudo snap install signal-desktop
+sudo snap install skype
+sudo snap install slack
+sudo snap install slack-term
+
+
+## Developer Tools ##
+
+sudo snap install android-studio --classic
+sudo snap install netbeans --classic
+sudo snap install pycharm-community
+
+
+## Misc Snaps ##
+
+sudo snap install ghex-udt       # GHex - Hex Editor
+sudo snap install gnome-twitch   # Twitch client
+sudo snap install googler        # Googler - Google Web, News and Search from CLI
+sudo snap install iagno          # Othello / Reversi
+sudo snap install mycroft        # MyCroft
