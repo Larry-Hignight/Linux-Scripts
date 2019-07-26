9f5250a391c79ae0835bc85e1a30c1b69578da8d
Larry-Hignight <larry.hignight@gmail.com>
2019-07-25 21:30:56 -0700

    Updated instructions for installing Google Earth.

--- apt-install.sh
+++ apt-install.sh
@@ -237,29 +237,3 @@ sudo apt -y install fraqtive         # Mandlebrot and Julia set viewer
 #sudo dpkg -i google-chrome-stable_current_amd64.deb
 #sudo apt -f install
 #google-chrome http://www.yahoo.com
-
-
-#echo "##################"
-#echo "## Google Earth ##"
-#echo "##################"
-## Try the following instructions instead: https://help.ubuntu.com/community/GoogleEarth
-
-#wget http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb #  64bit
-#sudo apt -y install libc6-i386 libglib2.0-0:i386 libsm6:i386 libglu1-mesa:i386 libgl1-mesa-glx:i386 libxext6:i386 libxrender1:i386 libx11-6:i386 libfontconfig1:i386 lsb-core
-
-##Still has an issue with ia32-libs not being installed, yet not installable using apt
-# Double-click the downloaded .deb package to install it using the Ubuntu Software Center.
-# After installation you should find Google Earth in the Applications -> Internet menu or through the Dash.
-
-## Old method:  Not working...
-#wget http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb #  64bit
-#sudo apt -y install lib32z1 lib32ncurses5 lib32bz2-1.0
-#sudo dpkg -i google-earth-stable*.deb
-#sudo apt -f install
-
-# Networking / Security
-#sudo apt -y install aircrack-ng john #kismet  ##kismet requires setuid root?!
-#wget ftp://mirrors.kernel.org/openwall/wordlists/languages/English/3-large/lower.gz
-#wget ftp://mirrors.kernel.org/openwall/wordlists/languages/Spanish/lower.gz
-#wget ftp://mirrors.kernel.org/openwall/wordlists/languages/French/lower.gz
-#wget ftp://mirrors.kernel.org/openwall/wordlists/languages/French/mixed.gz
---
+++ google-earth-install.sh
@@ -0,0 +1,7 @@
+echo "###################"
+echo "## Google Earth ##"
+echo "##################"
+
+sudo apt install gdebi-core wget
+wget https://dl.google.com/dl/linux/direct/google-earth-pro-stable_7.3.2_amd64.deb
+sudo gdebi google-earth-pro-stable_current_amd64.deb
